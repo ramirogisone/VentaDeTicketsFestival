9ea8cb24202f2a45fce42bff1a208624239680fb
ramirogisone <ramirogisone@gmail.com>
2021-03-11 22:38:19 +0100

    finalizo seccion de pases y aplico scroll view

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+});
 
+function scrollNav() {
+    const enlaces = document.querySelector('navegacion-principal a');
+
+    enlaces.forEach( function(enlace) {
+        enlace.addEventListener('click', function(ev){
+            ev.preventDefault();
+            const seccion = document.querySelector(e.target.attributes.href.value);
+
+            seccion.scrollIntoView({
+                behavior: 'smooth', 
+            })
+        })
+    });
+}
 document.addEventListener('DOMContentLoaded', function() {
     crearGaleria();
 });
